192f259f08bb32f1d0f1f77d512bd1f5e8b32f82
James Cox <jamesncox@yahoo.com>
2019-02-12 17:30:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
+def fizzbuzz(int)
+  if int / 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  elfif int % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts "nil"
+  end
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
