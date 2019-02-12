8c0abc0219a43b200ab1018ca76794c4a5a0fac2
James Cox <jamesncox@yahoo.com>
2019-02-12 17:41:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(int)
-  
-  if int % 3 == 0
+  if (int % 5) == 0 && (int % 3) == 0
+    puts 'FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 and int % 5 == 0
-    "FizzBuzz"
   else
     puts "nil"
   end
